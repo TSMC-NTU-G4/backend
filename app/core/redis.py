f95c2043c78fd5dd2d7253a11ac204e3e69c85a4
ongdisheng <ongdisheng@gmail.com>
2025-05-11 22:19:18 +0800

    fix lint issue

--- app/core/redis.py
+++ app/core/redis.py
@@ -2,7 +2,6 @@ import json
 import os
 from typing import TypeVar
 
-from redis.asyncio import Redis
 from dotenv import load_dotenv
 from pydantic import BaseModel, ValidationError
 from redis.asyncio import Redis
