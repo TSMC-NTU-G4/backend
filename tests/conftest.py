42a4e5caa6b38e5123b260b963eee1b67aa881cf
ongdisheng <ongdisheng@gmail.com>
2025-05-12 17:23:25 +0800

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,4 @@
 import sys
 from pathlib import Path
 
-import pytest
-from fastapi.testclient import TestClient
-
-from app.main import app
-
 sys.path.append(str(Path(__file__).resolve().parents[1]))
-
-
-@pytest.fixture
-def client() -> TestClient:
-    return TestClient(app)
