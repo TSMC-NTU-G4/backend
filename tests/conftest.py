2c72b633d8cf27b1c21f186246d334a1f5a19cea
ongdisheng <ongdisheng@gmail.com>
2025-05-12 17:21:50 +0800

    updated path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
+import sys
+from pathlib import Path
+
 import pytest
 from fastapi.testclient import TestClient
 
 from app.main import app
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 
 @pytest.fixture
